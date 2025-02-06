fd1c4b035075769274c2ed82481098de076b30fc
Junhwan Lee <105141262+junans0boi@users.noreply.github.com>
2025-02-06 14:05:25 +0900

    Create a bottom navigation bar
    Create a bottom navigation bar

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Home />} /> 
         <Route path="/home" element={<Home />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/todolist" element={<TodoList />} />
--- todo-list/src/BottomNav.jsx
+++ todo-list/src/BottomNav.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+
 import "./BottomNav.css";
 import "./FontAwesome";
 // FontAwesomIcon 컴포넌트를 사용하기 위해 import
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const BottomNav = () =>{
-    return(
+const BottomNav = () => {
+    // 선택된 아이콘을 관리하는 state
+    const [activeNav, setActiveNav] = useState(1);
+    return (
         <nav className="wrapper">
             <div>
-                <FontAwesomeIcon icon="home" />
+                <Link to="/" className="nav-link" onClick={() => setActiveNav(1)}>
+                    <FontAwesomeIcon icon="home" className={activeNav === 1 ? "nav-item active" : "nav-item"} />
+                </Link>
             </div>
             <div>
-                <FontAwesomeIcon icon="calendar" />
+                <Link to="/calendar" className="nav-link" onClick={() => setActiveNav(2)}>
+                    <FontAwesomeIcon icon="calendar" className={activeNav === 2 ? "nav-item active" : "nav-item"} />
+                </Link>
             </div>
             <div>
-                <FontAwesomeIcon icon="list" />
-                
+                <Link to="/todolist" className="nav-link" onClick={() => setActiveNav(3)}>
+                    <FontAwesomeIcon icon="list" className={activeNav === 3 ? "nav-item active" : "nav-item"} />
+                </Link>
             </div>
             <div>
-                <FontAwesomeIcon icon="book" />
+                <Link to="/diary" className="nav-link" onClick={() => setActiveNav(4)}>
+                    <FontAwesomeIcon icon="book" className={activeNav === 4 ? "nav-item active" : "nav-item"} />
+                </Link>
             </div>
             <div>
-                <FontAwesomeIcon icon="ellipsis" />
+                <Link to="/more" className="nav-link" onClick={() => setActiveNav(5)}>
+                    <FontAwesomeIcon icon="ellipsis" className={activeNav === 5 ? "nav-item active" : "nav-item"} />
+                </Link>
             </div>
         </nav>
     );
