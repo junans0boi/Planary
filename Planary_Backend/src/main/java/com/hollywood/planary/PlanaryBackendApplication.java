ec75ebec0f8691de23255f2caba673516cc66671
Junhwan Lee <105141262+junans0boi@users.noreply.github.com>
2025-07-11 21:35:26 +0900

    add spring boot project

---
+++ Planary_Backend/src/main/java/com/hollywood/planary/PlanaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hollywood.planary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanaryBackendApplication.class, args);
+	}
+
+}
---
+++ Planary_Backend/src/test/java/com/hollywood/planary/PlanaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollywood.planary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
